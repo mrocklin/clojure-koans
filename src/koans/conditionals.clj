ade993f7558f3a4c8e2242c3889e7f96750f7ae2
Matthew Rocklin <mrocklin@gmail.com>
2012-04-21 17:33:09 -0500

    working on koans

--- src/koans/conditionals.clj
+++ src/koans/conditionals.clj
@@ -9,22 +9,22 @@
 
 (meditations
   "You will face many decisions"
-  (= __ (if (false? (= 4 5))
+  (= :a (if (false? (= 4 5))
           :a
           :b))
 
   "Some of them leave you no alternative"
-  (= __ (if (> 4 3)
+  (= [] (if (> 4 3)
           []))
 
   "And in such a situation you may have nothing"
-  (= __ (if (nil? 0)
+  (= nil (if (nil? 0)
           [:a :b :c]))
 
   "In others your alternative may be interesting"
   (= :glory (if (not (empty? ()))
               :doom
-              __))
+              :glory))
 
   "You may have a multitude of possible paths"
   (let [x 5]
